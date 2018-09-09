c09d263906b4459133ffd0d2fa93f5b77281f4a1
Luca5slose <hit_slose@163.com>
2018-09-10 00:53:57 +0800

    By L5

--- SocketTest/Server_v01.py
+++ SocketTest/Server_v01.py
@@ -6,13 +6,13 @@ def severTest():
     #第一个参数表示ipv4，第二个表示UDP
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+    # 绑定本机和10086端口
     addr = ('127.0.0.1', 10086)
     sock.bind(addr)
-    #绑定本机和10086端口
 
+    # 接收信息，参数是缓冲区大小，信息格式是tuple，前一个数据，后一个地址
+    # 注意信息流是byte格式，收发都要编码,默认是UTF-8
     data, adr = sock.recvfrom(500)
-    #接收信息，参数是缓冲区大小，信息格式是tuple，前一个数据，后一个地址
-    #注意信息流是byte格式，收发都要编码,默认是UTF-8
     text = data.decode()
     print(text)
 
